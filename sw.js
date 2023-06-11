0cda94337aba3cbc4bb3853b5f80ed1206e3ae7b
NobinPegasus <zeshanahmednobin@gmail.com>
2023-06-11 17:11:59 +0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3e163a8ac2686e91fb31.js"
+    "url": "webpack-runtime-0c7b1764930086a117d3.js"
   },
   {
     "url": "framework-e65ce11b871bf77a3d80.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bad44828bb71ea5ad68553844427eccf"
+    "revision": "3d3a5279575c8df49ab9016c634e6018"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-819fe14f87f7d11ff78a.js"
